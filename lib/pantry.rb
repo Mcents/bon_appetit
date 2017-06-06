9b84e45b9450d0b168bb87bc8af24356c3ff243f
Michael Centrelli <centm860@newschool.edu>
2017-06-06 10:50:17 -0600

    Getting there

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -17,39 +17,27 @@ class Pantry
   end
 
   def convert_units(recipe)
-    new_hash = {}
+    new_hash = a = Hash.new{ |h,k| h[k] = Hash.new(0)}
     recipe.ingredients.each do |key, value|
       if value < 1
-        new_hash[key] = "quantity"
-        new_hash[value / 100] = "Mili-Units"
+        new_hash[key][:quntity] = [value / 1000]
+        new_hash[key][:units] = ["Mili-Units"]
+        # new_hash[value / 1000] = "Mili-Units"
       elsif value > 100
-        new_hash[key] = "quantity"
-        new_hash[value / 100] = "Centi-Units"
+        new_hash[key][:quntity] = [value / 100]
+        new_hash[key][:units] = ["Mili-Units"]
+        # new_hash[value] = :quantity
+        # new_hash[value / 100] = "Centi-Units"
       else
-        new_hash[key] = "quantity"
-        new_hash[value] = "Universal-Units"
+        new_hash[key][:quntity] = [value]
+        new_hash[key][:units] = ["Mili-Units"]
+        # new_hash[value] = :quantity
+        # new_hash[value] = "Universal-Units"
       end
     end
     new_hash
   end
 
 
-
-  # def convert_units(recipe)
-  #   new_hash = {}
-  #   recipe.ingredients.each do |key, value|
-  #     if value < 1
-  #       new_hash[key] = "quantity"
-  #       new_hash[value / 1000] = "Mili-Units"
-  #     elsif value > 100
-  #       new_hash[key] = "quantity"
-  #       new_hash[value / 100] = "Centi-Units"
-  #     else
-  #       new_hash[key] = "quantity"
-  #       new_hash[value] = "Universal-Units"
-  #     end
-  #   end
-  #   new_hash
-  # end
 
 end
