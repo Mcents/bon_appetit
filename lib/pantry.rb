fdd01771f12b996627b043e0e7620098788c5e85
Michael Centrelli <centm860@newschool.edu>
2017-06-06 10:56:55 -0600

    Holy shit it works

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -20,17 +20,17 @@ class Pantry
     new_hash = a = Hash.new{ |h,k| h[k] = Hash.new(0)}
     recipe.ingredients.each do |key, value|
       if value < 1
-        new_hash[key][:quntity] = [value / 1000]
-        new_hash[key][:units] = ["Mili-Units"]
+        new_hash[key][:quantity] = (value * 1000)
+        new_hash[key][:units] = "Milli-Units"
         # new_hash[value / 1000] = "Mili-Units"
       elsif value > 100
-        new_hash[key][:quntity] = [value / 100]
-        new_hash[key][:units] = ["Mili-Units"]
+        new_hash[key][:quantity] = value / 100
+        new_hash[key][:units] = "Centi-Units"
         # new_hash[value] = :quantity
         # new_hash[value / 100] = "Centi-Units"
       else
-        new_hash[key][:quntity] = [value]
-        new_hash[key][:units] = ["Mili-Units"]
+        new_hash[key][:quantity] = value
+        new_hash[key][:units] = "Universal Units"
         # new_hash[value] = :quantity
         # new_hash[value] = "Universal-Units"
       end
